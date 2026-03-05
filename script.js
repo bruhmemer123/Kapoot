8a47ede409667f9f92b688f8d7bace4ca285143a
Affan <afydking@gmail.com>
2026-03-06 00:36:49 +0530

    added basic summary page,fixed qntime delay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentQuestion=Math.floor(Math.random()*10)
+let currentQuestion=0
 let points=0
 let questionCount=0
 const maxQuestions=10
@@ -6,6 +6,10 @@ let totalTimeRemaining=60
 let qnTimeRemaining=10
 let totalTimer=null
 let qnTimer=null
+let correctans=[]
+let wrongans=[]
+let qnno=1
+let tempquestions=[]
 function startTotalTimer(){
     totalTimer=setInterval(()=>{
         totalTimeRemaining--
@@ -19,11 +23,23 @@ function startTotalTimer(){
 
 }
 function startGame(){
+    tempquestions = [...questionsData]
+    clearInterval(totalTimer)
+    clearInterval(qnTimer)
     let menu=document.getElementById("menu")
     let questions=document.getElementById("questions")
+    qnno=1
+    points=0
+    questionCount=0 
+    correctans=[]
+    wrongans=[]
     menu.classList.add("hide")
     questions.classList.remove("hide")
-    totalTimeRemaining=60
+    document.getElementById("restart").classList.add("hide")
+    document.getElementById("correctans").classList.add("hide")
+    document.getElementById("wrongans").classList.add("hide")
+    totalTimeRemaining=61
+    document.querySelector(".totaltimer").textContent = "Total time:" + totalTimeRemaining
     startTotalTimer()
     loadQuestion()
 
@@ -34,11 +50,12 @@ function loadQuestion(){
     document.getElementById("correct").classList.add("hide")
     document.getElementById("incorrect").classList.add("hide")
     document.querySelector(".points").textContent="Points:"+points
-    if(questionCount>=maxQuestions||questionsData.length===0){
+    if(questionCount>=maxQuestions||tempquestions.length===0){
         endGame()
         return
     }
     qnTimeRemaining=10
+    document.querySelector(".qntimer").textContent = "Question time:" + qnTimeRemaining
     clearInterval(qnTimer)
     qnTimer=setInterval(()=>{
         qnTimeRemaining--
@@ -47,14 +64,14 @@ function loadQuestion(){
             clearInterval(qnTimer)
             points-=2
             document.getElementById("incorrect").classList.remove("hide")
-            questionsData.splice(currentQuestion,1)
+            tempquestions.splice(currentQuestion,1)
             questionCount++;
             loadQuestion()
         }
     },1000)
     
-    currentQuestion=Math.floor(Math.random() * questionsData.length)
-    let q=questionsData[currentQuestion]
+    currentQuestion=Math.floor(Math.random() * tempquestions.length)
+    let q=tempquestions[currentQuestion]
     document.querySelector(".question").textContent=q.question
     let buttons=document.querySelectorAll(".answers button")
     buttons.forEach(btn => btn.disabled = false)
@@ -65,23 +82,29 @@ function loadQuestion(){
             clearInterval(qnTimer)
             if(index===q.correct){
                 points+=5
+                correctans.push(qnno++)
                 document.getElementById("correct").classList.remove("hide")
             } else {
                 points-=2
+                wrongans.push(qnno++)
                 document.getElementById("incorrect").classList.remove("hide")
             }
             console.log(points)
-            questionsData.splice(currentQuestion,1)
-            questionCount++;
-            setTimeout(loadQuestion, 300);
+            tempquestions.splice(currentQuestion,1)
+            questionCount++
+            setTimeout(loadQuestion, 300)
         }
     })
 }
 
 function endGame(){
-    clearInterval(totalTimer);
-    clearInterval(qnTimer);
-
-    document.getElementById("questions").classList.add("hide");
-    alert("Game Over! Final Score: " + points);
+    clearInterval(totalTimer)
+    clearInterval(qnTimer)
+    document.querySelector(".correctans").textContent=`Correct answers:`+correctans.join(", ")
+    document.getElementById("correctans").classList.remove("hide")
+    document.querySelector(".wrongans").textContent=`Wrong answers:`+wrongans.join(", ")
+    document.getElementById("wrongans").classList.remove("hide")
+    document.getElementById("questions").classList.add("hide")
+    alert("Game Over! Final Score: " + points)
+    document.getElementById("restart").classList.remove("hide")
 }
